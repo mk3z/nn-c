f05a7b3fbd99dcd720af639351caf8aa6334847b
Matias Zwinger <matias.zwinger@csc.fi>
2024-06-14 13:13:41 +0300

    Add nix support

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "nn";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "nn";
+          src = ./nn;
+        };
+      }
+    );
+}
